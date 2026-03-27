1640ca767d07a092eaad66dd9d95a4ec1c92546f
Wania Faraz <waniafarazk@gmail.com>
2026-03-27 15:23:03 -0400

    fixed missing instruction

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const PORT = 8000;
 //INSTRUCTIONS FOR SERVER AND WEBSITE ACCESS:
 //For visual studio code:
 //Open terminal and write: "node server.js" (without the quotes)
+//After any modifications to the file, you will have to kill the server (CTRL + C)
+//     and restart it, and then refresh the web page to see changes
 //Open a browser (chrome, edge, etc...) and type one of the following urls:
 // localhost:8000/student/home      <---- leads to the student home page
 // localhost:8000/instructor/home   <---- leads to the instructor home page
 //to navigate between webpages, can change the url or click on whatever needs to be accessed
-//for possible url names, see student.routes.js or instructor.routes.js
+//for possible url names, see files "student.routes.js" or "instructor.routes.js"
 //==============================================================================================================
 
 
