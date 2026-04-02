057a22fb5be96e85f9b0af9e5a163e7fc27a9eb0
Wania Faraz <waniafarazk@gmail.com>
2026-04-02 12:41:58 -0400

    Added course generation and fixed select dropdown generation

--- public/scripts/instructor_home_page_scripts.js
+++ public/scripts/instructor_home_page_scripts.js
@@ -1,10 +1,63 @@
 console.log('scripts loaded');
-//document.addEventListener('DOMContentLoaded', getSession); //do getSession when the document loads
+document.addEventListener('DOMContentLoaded', getSession);
 console.log("reached scripts");
 
-const PORT = 8080;
+
 
 let userId;
+let response; //for whenever fetch is used
+
+//get instructorId
+async function getSession() {
+    response = await fetch("/api/instructor/session");
+    const session = await response.json();
+    if(!session) {
+        response = await fetch("/instructor/sign-in");
+    }
+    userId = session.userId;
+    await loadHomePage();
+}
+
+//load the home page with the courses of the instructor
+async function loadHomePage() {
+    const instructorId = userId;
+    response = await fetch(`/api/instructor/get-courses/${instructorId}`); 
+    instructor_courses = await response.json(); //JSON: instructorId and courseId
+    await instructor_courses.forEach( async (value, index, array) => {
+        //get courses from courseIds
+        const courseId = value.courseId;
+        response = await fetch(`/api/instructor/get-course-from-id/${courseId}`);
+        const [course] = await response.json();
+        //get course info from JSON object
+        const title = course.title;
+        const code = course.code;
+        const section = course.section;
+        await addCourseToHomePage(code, title, section);
+    })
+}
+    
+
+
+//funtion to add course to home page
+async function addCourseToHomePage(code, title, section) {
+    const courseArea = document.getElementById("course-area");
+    courseArea.innerHTML += `<div class="course">
+                                <div class="course-image course-img-1"></div>
+                                <a class="course-info-text" href="course-page">
+                                    <p class="course-code">${code}</p>
+                                    <p class="course-name">${title}</p>
+                                    <p class="course-section">Section ${section}</p>
+                                </a>
+                                <div class="course-toggle-row">
+                                    <span>Course visible to students</span>
+                                    <label class="toggle-switch">
+                                        <input type="checkbox" checked />
+                                        <span class="toggle-slider"></span>
+                                    </label>
+                                </div>
+                            </div>`
+}
+
 
 //MODAL FOR ADDING COURSES:
 const addCoursesModalButton = document.getElementById("add-courses-modal-button");
@@ -20,6 +73,51 @@ closeModalButton.addEventListener('click', function() {
     const modal = document.getElementById('add-course-modal');
     modal.close();
 })
+
+
+//WHEN THE USER IS ENTERING A COURSE CODE - CHECK WHICH SECTIONS EXIST FOR THIS COURSE CODE - CREATE ARRAY OF SECTIONS
+const courseCodeInput = document.getElementById("ask-code");
+courseCodeInput.addEventListener('keyup', async function() {
+    if(courseCodeInput.value.length == 8) { //a valid course code should have 8 characters: 'AAAA 111'
+        //fetch the route to get course sections from ids
+        //remove spaces from the course code - to avoid errors
+        const courseCode = courseCodeInput.value.slice(0,4) + courseCodeInput.value.slice(5);
+        const response = await fetch(`/api/instructor/get-sections-from-course-code/${courseCode}`);
+        const sections = await response.json(); //already available sections for the course code entered
+        const element = document.getElementById("course-select-section");
+        generateSectionsDropDown(sections, element);
+    }
+})
+//GENERATE THE SECTION DROPDOWN
+async function generateSectionsDropDown(sections, element) {
+    //sections is an array of sections
+    //element is the html element where the dropdown needs to be inserted
+    //Remove the sections that are already taken for this course code
+    let allowedSections;
+    if(sections.length == 0) { //invalid course, i.e. has no sections
+        element.innerHTML += `<option >Invalid</option>`;
+    }
+    const defaultAllSections = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    console.log("defaultAllSections:", defaultAllSections);
+    //FILTER ALLOWED SECTIONS
+    const allowedSectionsPromise = defaultAllSections.filter( (value, index, array) => {
+        return !(sections.includes(value));
+    })
+    allowedSections = await Promise.all(allowedSectionsPromise);
+    allowedSections.forEach((value, index, array) => {
+        element.innerHTML += `<option value=${value}>${value}</option>`;
+    })
+}
+
+
+
+
+
+
+
+
+
+
 
 /** TECHNICALLY FOR STUDENTS...MOVE THERE
 //GENERATE DROPDOWN OPTIONS FOR THE COURSE SECTION IN THE MODAL FOR WHEN A COURSE IS ADDED
@@ -55,27 +153,3 @@ async function generateSectionsDropDown(sections, element) {
 
 
 
-/**
-//WAITING FOR INSTRUCTOR SESSION AND SIGN IN IS DEFINED
-async function getSession() {
-    const session = await fetch();
-    userId = session.userId;
-    loadHomePage();
-}
-    
-
-async function loadHomePage() {
-    await loadCourses();
-}
-
-async function loadCourses() {
-    //temmporarily hardcode instructor id
-    const instructorId = 2001;
-    const response = await fetch(`${URL}/api/instructor/get-courses/${instructorId}`);
-    const courseIds = await response.json();
-    const courses = courseIds.forEach( async (value, index, array) => {
-        response = await fetch(`${URL}/api/instructor`)
-    })
-    
-}
-    */
